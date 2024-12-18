f52e4ba9739c667ecec848802dd2f959242f7106
IggyTown <isabeltownsend001@gmail.com>
2024-12-18 12:28:09 +0000

    added additional html for the select option

--- src/index.js
+++ src/index.js
@@ -67,7 +67,7 @@ function updateCity(event) {
       <h2>${cityName}</h2>
       <div class="date">${cityTime.format("MMMM	Do YYYY")}</div>
     </div>
-    <div class="time">${cityTime.format("h:mm:ss")} <small>${cityTime.format(
+    <div class="time">${cityTime.format("H:mm:ss")} <small>${cityTime.format(
     "A"
   )}</small></div>
   </div>
